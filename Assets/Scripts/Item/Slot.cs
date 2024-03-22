554ea604a1aebfd778d7cf538b9562be98111ca9
Hyuns008 <gustn4984@naver.com>
2024-03-22 16:09:11 +0900

    인벤토리 추가

--- Assets/Scripts/Item/Slot.cs
+++ Assets/Scripts/Item/Slot.cs
@@ -1,12 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Slot : MonoBehaviour
 {
     [SerializeField] private int itemIndex;
     [SerializeField] private int slotQuantity;
     private bool maxItem = false;
+
+    [SerializeField] private Image itemImage;
+    [SerializeField] private TMP_Text itemQuantityText;
+
+    private void Start()
+    {
+        if (itemIndex == 0)
+        {
+            itemQuantityText.text = "";
+        }
+    }
 
     private void Update()
     {
@@ -28,6 +41,18 @@ public class Slot : MonoBehaviour
 
             maxItem = false;
         }
+
+        if (itemIndex == 0)
+        {
+            if (slotQuantity == 0)
+            {
+                return;
+            }
+
+            itemImage.sprite = null;
+            slotQuantity = 0;
+            itemQuantityText.text = slotQuantity.ToString();
+        }
     }
 
     public void SetSlot(int _itemIndex, GameObject _itemObj)
@@ -41,10 +66,19 @@ public class Slot : MonoBehaviour
         {
             itemIndex = _itemIndex;
             slotQuantity = 1;
+            SpriteRenderer itemSpR = _itemObj.GetComponent<SpriteRenderer>();
+            Sprite itemSr = itemSpR.sprite;
+            itemImage.sprite = itemSr;
+            itemQuantityText.text = slotQuantity.ToString();
+            Destroy(_itemObj);
         }
         else
         {
+            SpriteRenderer itemSpR = _itemObj.GetComponent<SpriteRenderer>();
+            Sprite itemSr = itemSpR.sprite;
+            itemImage.sprite = itemSr;
             ++slotQuantity;
+            itemQuantityText.text = slotQuantity.ToString();
             Destroy(_itemObj);
         }
     }
